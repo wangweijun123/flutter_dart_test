83cfcd3f79529aa1f82dc4e017b5526b89d14f2f
jntm <262448350@qq.com>
2023-09-07 17:38:53 +0800

    flutter AbsorbPointer 事件拦截

--- player/lib/videoplayer/video_player_page.dart
+++ player/lib/videoplayer/video_player_page.dart
@@ -23,13 +23,45 @@ class _VideoViewState extends State<VideoView> {
 
   @override
   Widget build(BuildContext context) {
+    print('build onTapVideo ...');
     return Scaffold(
         appBar: AppBar(title: Text("Fijkplayer Example")),
-        body: Container(
-          alignment: Alignment.center,
-          child: FijkView(
-            player: widget.player,
-          ),
+        body: GestureDetector(
+          onTap: onTapVideo,
+          child: Stack(children: [
+            // 注意这个对事件的处理
+            AbsorbPointer(
+              absorbing: true,
+              child: FijkView(
+                player: widget.player,
+              ),
+            ),
+
+            Container(
+              alignment: Alignment.center,
+              child: FijkView(
+                player: widget.player,
+              ),
+            ),
+
+            // center 占满了整个屏幕(除去appbar)
+            // Center(
+            //     child: Image.asset(
+            //   'packages/player/assets/play.png',
+            //   width: 70,
+            //   height: 70,
+            // )),
+
+            // Align 也是占满了整个屏幕(除去appbar)
+            if (widget.player.state == FijkState.paused)
+              Align(
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    'packages/player/assets/play.png',
+                    width: 70,
+                    height: 70,
+                  )),
+          ]),
         ));
   }
 
@@ -37,5 +69,16 @@ class _VideoViewState extends State<VideoView> {
   void dispose() {
     super.dispose();
     widget.player.release();
+  }
+
+  void onTapVideo() {
+    print('onTapVideo ...');
+    if (widget.player.state == FijkState.paused) {
+      widget.player.start();
+    } else {
+      widget.player.pause();
+    }
+
+    setState(() {});
   }
 }
